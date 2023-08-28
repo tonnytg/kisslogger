924f5dae88523e4f95e2182abc8f1f813b79fa4f
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-08-27 23:59:22 -0300

    feat: implement global error

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"errors"
 	"log/slog"
+	"os"
 )
 
+var globalError error
+
 func main() {
-	slog.Info("Start kisslogger", "errors:", nil)
+	slog.Info("Start kisslogger", "errors:", globalError)
+
+	if len(os.Args) < 2 {
+		globalError = errors.New("No arguments")
+		slog.Error("main.go", "errors:", globalError)
+		return
+	}
+
+	slog.Info("End kisslogger", "errors:", globalError)
 }
